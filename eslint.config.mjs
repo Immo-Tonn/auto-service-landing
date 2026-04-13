d2f86be7eddf13ffeba42a62dcc96ae0ce0f17b3
Sa-Ev <sa-ev@gmx.net>
2026-04-13 02:52:34 +0200

    chore: setup eslint flat config and fix linting issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,13 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "types/**/*",
   ]),
+    {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
