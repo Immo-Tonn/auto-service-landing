1f2cd7aea7c0fb5541d25431dad0ec970ca20d87
Alexander Ytvtushenko <Sa-Ev@gmx.net>
2026-04-27 02:43:47 +0200

    build-folder remoted

--- CUsersICHauto-service-landing/.next/dev/static/chunks/src_app_about_page_tsx_002ye8~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0ili0.2._.js","static/chunks/src_app_about_01s4cc6._.js"],
-    source: "dynamic"
-});
